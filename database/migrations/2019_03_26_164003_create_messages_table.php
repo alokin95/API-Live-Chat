b42a7b1de5dae02c36d7c9d142524447012c5d5a
Nikola Mitrovic <nikola_m95@hotmail.com>
2019-03-26 18:32:57 +0100

    added model and migrations

---
+++ database/migrations/2019_03_26_164003_create_messages_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('from');
+            $table->unsignedBigInteger('to');
+            $table->text('message');
+            $table->timestamps();
+
+
+            $table->foreign('from')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('to')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
