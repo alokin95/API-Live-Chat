4f8587ecdf7b2a67fdf40916b836caed1ec1fa51
Nikola Mitrovic <nikola_m95@hotmail.com>
2019-03-28 18:34:59 +0100

    Login added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AuthController extends Controller
 {
     public function store()
     {
-        dd(request());
+
+        $validated = \Validator::make(request()->user, [
+            'username' => 'required|min:3|unique:users,username',
+            'password' => 'required|min:5|max:30|confirmed'
+        ])->validate();
+
+        $validated['password'] = bcrypt($validated['password']);
+
+        $user = new User($validated);
+
+        if ($user->save())
+        {
+            $token = auth()->login($user);
+
+            return response()->json([
+                'token' => $token
+            ], 201);
+        }
+
+    }
+
+    public function login()
+    {
+        $token = auth()->attempt([
+            'username' => request('user')['username'],
+            'password' => request('user')['password']
+        ]);
+
+        if ($token)
+        {
+            return response()
+                ->json([
+                    'token' => $token
+                ], 200);
+        }
     }
 }
