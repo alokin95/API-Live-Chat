e368f5db887456c27c95cbf944c413a1192030ea
Nikola Mitrovic <nikola_m95@hotmail.com>
2019-03-28 18:34:21 +0100

    Login and register routes

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/', 'HomeController@index');
 Route::post('/users', 'AuthController@store');
+Route::post('/login', 'AuthController@login');
